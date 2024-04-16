b2696f5b7b7b6366c625bb761bdab63638fd6f4a
Nishant Jha <nishantjha360@gmail.com>
2024-04-16 13:58:21 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+# Use an official Nginx image as the base image
+FROM nginx:latest
+
+# Copy the HTML, CSS, JavaScript, and any other assets into the container
+COPY index.html /usr/share/nginx/html/
+COPY script.js /usr/share/nginx/html/
+
+# Expose port 80 (default HTTP port)
+EXPOSE 80
---
+++ script.js
@@ -0,0 +1,28 @@
+// script.js
+
+// Function to fetch data from API
+async function fetchData(apiUrl, elementId) {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        // Get the predicted_value from the API response
+        const predictedValue = data.predicted_value;
+
+        // Update the HTML element with the predicted value
+        const element = document.getElementById(elementId);
+        element.textContent = `${predictedValue}`;
+        console.log(`GET request to ${apiUrl} succesfull`)
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+// Call fetchData for each API when the page is loaded
+window.onload = async () => {
+    // API 1
+    await fetchData('https://aqi-forecast-api-production.up.railway.app/predict/?pm_type=pm2p5', 'pm2p5'); 
+
+    // API 2
+    await fetchData('https://aqi-forecast-api-production.up.railway.app/predict/?pm_type=pm10', 'pm10'); 
+};
